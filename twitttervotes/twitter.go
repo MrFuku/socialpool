7474435454fe7791f54303cf8304edd8ac743332
MrFuku <mmafighter_shun@icloud.com>
2020-03-22 14:20:46 +0900

    request生成関数実装

--- twitttervotes/twitter.go
+++ twitttervotes/twitter.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -54,4 +55,25 @@ func setupTwitterAuth() {
 			Secret: os.Getenv("SP_TWITTER_SECRET"),
 		},
 	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Authorization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
 }
