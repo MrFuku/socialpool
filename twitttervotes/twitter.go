20105cfb834042d340055accb52ba490c7477539
MrFuku <mmafighter_shun@icloud.com>
2020-03-22 16:35:24 +0900

    TwitterStream読み込みgoroutineの生成関数実装

--- twitttervotes/twitter.go
+++ twitttervotes/twitter.go
@@ -123,3 +123,23 @@ func readFromTwitter(votes chan<- string) {
 		}
 	}
 }
+
+func startTwitterStream(stopchan <-chan struct{}, votes chan<- string) <-chan struct{} {
+	stoppedchan := make(chan struct{}, 1)
+	go func() {
+		defer func() {
+			stoppedchan <- struct{}{}
+		}()
+		for {
+			select {
+			case <-stopchan:
+				log.Println("Twitterへの問い合わせを終了します...")
+				return
+			default:
+				log.Println("Twitterに問い合わせます...")
+				readFromTwitter(votes)
+			}
+		}
+	}()
+	return stoppedchan
+}
